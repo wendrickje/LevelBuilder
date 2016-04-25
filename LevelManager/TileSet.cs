f5710e42564607fbeb01e7ab61494ec56db96a00
Jeromy Wendrick <wendrickje@gmail.com>
2016-04-25 12:26:18 -0400

    init commit

---
+++ LevelManager/TileSet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace LevelManager
+{
+    [DataContract]
+    public class TileSet
+    {
+        [DataMember(Order = 0)]
+        public string Resource { get; set; }
+
+        [DataMember(Order = 1)]
+        public int TileSize { get; set; }
+
+        [DataMember(Order = 2)]
+        public int Rows { get; set; }
+
+        [DataMember(Order = 3)]
+        public int Columns { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember(Order = 4)]
+        public string LevelLayout { get; set; }
+
+    }
+}
